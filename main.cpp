ef01ad68e33f41625ef93b9a071eb349793505c4
44982432 <fogleins@hotmail.hu>
2020-05-13 22:05:49 +0200

    Tesztesetek hozzáadása

--- main.cpp
+++ main.cpp
@@ -11,51 +11,132 @@
 #define MEMTRACE
 /* tesztesetek */
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    // fájlba és konzolra írás tesztelése mindegyik filmtípuson
+    TEST(movies, print) {
+        stringstream t1, t2, t3;
+        Movie test1mv("1917", 119, 2019);
+        Family test2mv("Frozen 2", 103, 2019, ages6AndUp);
+        Documentary test3mv("Free solo", 97, 2018, "Professional rock climber "
+                                                   "Alex Honnold attempts to conquer the first free solo climb of "
+                                                   "famed El Capitan's 900-metre vertical rock face at Yosemite "
+                                                   "National Park.");
+        // konzolra írás függvényének tesztelése
+        test1mv.print(t1);
+        // az id inicializálatlan (unsigned -1 = 4294967295)
+        EXPECT_STREQ("4294967295\t1917\t119\t2019\tMovie", t1.str().c_str());
 
-    Movie film1 = Movie("Film cime", 120, 2020);
-    Family film2 = Family("Csaladi film", 108, 2018, ages12AndUp);
-    Documentary film3 = Documentary("Dokumentumfilm", 86, 2013,"Dokumentumfilm leirasa");
-    Movie film4 = Movie("Film cime", 92, 2020);
-    film4 = film1;
-    bool egyezik = film1 == film3;
-    Collection coll;
-    coll.add(film1);
-    coll.add(film2);
-    coll.add(film3);
-    coll.remove(10);
-    coll.print();
-    coll.writeFile("./teszt.txt");
-    coll.search("ci");
-    coll.remove(2); // TODO: ha ez a sor nincs itt, a coll2 rész crashel
-    coll.print();
-    cout << coll.getMovies().getElementCount() << endl;
+        // gyűjteményhez adjuk, hogy kapjon id-t
+        Collection coll;
+        coll.add(test2mv);
+        test2mv.print(t2);
+        EXPECT_STREQ("0\tFrozen 2\t103\t2019\tFamily\t6", t2.str().c_str());
 
-    coll.clearCollection();
+        // fájlba írás
+        test3mv.print(t3, true);
+        EXPECT_STREQ("1;Free solo;97;2018;Professional rock climber Alex Honnold attempts to conquer the "
+                     "first free solo climb of famed El Capitan's 900-metre vertical rock face at "
+                     "Yosemite National Park.", t3.str().c_str());
+    } ENDM
 
-    cout << "\ncoll2:\n";
-    Collection coll2;
-    coll2 = coll;
-    coll.clearCollection();
-    if (!egyezik)
-        coll2.print();
+    // gyűjteményhez adás és kiírás
+    TEST(collection, addAndPrint) {
+        Movie test1mv("1917", 119, 2019);
+        Family test2mv("Frozen 2", 103, 2019, ages6AndUp);
+        Documentary test3mv("Free solo", 97, 2018, "Professional rock climber "
+                                                   "Alex Honnold attempts to conquer the first free solo climb of "
+                                                   "famed El Capitan's 900-metre vertical rock face at Yosemite "
+                                                   "National Park.");
+        Collection coll;
+        //üres gyűjtemény kiírása
+//        EXPECT_STREQ("ID\tCim\tHossz\tEv\tKategoria\tEgyeb", coll.print());
+        coll.add(test1mv);
+        coll.add(test2mv);
+        coll.add(test3mv);
+        EXPECT_EQ(3, coll.getMovies().getElementCount());
+        stringstream t1, t2, t3;
 
-    // beolvasás
-    cout << endl << endl << "Beolvasas" << endl;
-    Collection coll3;
-    coll3.readFile("./teszt.txt");
-    coll3.print(1);
-    coll3.print();
+        test1mv.print(t1);
+        EXPECT_STREQ("0\t1917\t119\t2019\tMovie", t1.str().c_str());
+        test2mv.print(t2);
+        EXPECT_STREQ("1\tFrozen 2\t103\t2019\tFamily\t6", t2.str().c_str());
+        test3mv.print(t3, true);
+        EXPECT_STREQ("1;Free solo;97;2018;Professional rock climber Alex Honnold attempts to conquer the "
+                     "first free solo climb of famed El Capitan's 900-metre vertical rock face at "
+                     "Yosemite National Park.", t3.str().c_str());
+    } ENDM
 
-    cout << "Parameter nelkuli ctor" << endl;
-    // TODO: paraméter nélkül hívható ctor
-    Movie asdf;
-    asdf.print();
-    cout << endl;
+    // a filmek operator= és operator== függvényeinek tesztelése
+    TEST(movies, operators) {
+        Movie test1mv("1917", 119, 2019);
+        Family test2mv("Frozen 2", 103, 2019, ages6AndUp);
+        Documentary test3mv("Free solo", 97, 2018, "Professional rock climber "
+                                                   "Alex Honnold attempts to conquer the first free solo climb of "
+                                                   "famed El Capitan's 900-metre vertical rock face at Yosemite "
+                                                   "National Park.");
+        Movie test1copy = test1mv;
+        Family test2copy = test2mv;
+        Documentary test3copy = test3mv;
+//        Documentary wrongType = test2mv;
+//        EXPECT_EQ(false, test2mv == wrongType);
+        EXPECT_EQ(false, test1mv == test2mv);
+        EXPECT_EQ(false, test3mv == test1mv);
+        EXPECT_EQ(true, test1mv == test1copy);
+        EXPECT_EQ(true, test2mv == test2copy);
+        EXPECT_EQ(true, test3mv == test3copy);
+    } ENDM
 
-    Documentary asdfg;
-    asdfg.print();
-    cout << endl;
+    // a túlindexelésnél dobott kivételt és a törlés függvényeket teszteli
+    TEST(collection, indexer) {
+        Movie test1mv("1917", 119, 2019);
+        Family test2mv("Frozen 2", 103, 2019, ages6AndUp);
+        Documentary test3mv("Free solo", 97, 2018, "Professional rock climber "
+                                                   "Alex Honnold attempts to conquer the first free solo climb of "
+                                                   "famed El Capitan's 900-metre vertical rock face at Yosemite "
+                                                   "National Park.");
+        Collection coll;
+        coll.add(test1mv);
+        coll.add(test2mv);
+        coll.add(test3mv);
+        EXPECT_EQ(3, coll.getMovies().getElementCount());
+        EXPECT_ANY_THROW(coll.getMovies()[15]);
+        EXPECT_NO_THROW(coll.getMovies()[2]);
+        EXPECT_NO_THROW(coll.remove(2));
+        EXPECT_EQ(2, coll.getMovies().getElementCount());
+        coll.clearCollection();
+        EXPECT_EQ(0, coll.getMovies().getElementCount());
+    } ENDM
+
+    // keresés a gyűjteményben
+    TEST(collection, search) {
+        Collection coll;
+        stringstream ss1, ss2;
+        Movie mv1("1917", 119, 2019);
+        Family mv2("Frozen 2", 103, 2019, ages6AndUp);
+        Documentary mv3("Free solo", 97, 2018, "Alex Honnold attempts to conquer "
+                                               "the first free solo climb of El Capitan's 900-metre vertical rock face.");
+        coll.add(mv1);
+        coll.add(mv2);
+        coll.add(mv3);
+        coll.search("oze", ss1);
+        EXPECT_STREQ("Kereses - \"oze\":\nID\tCim\tHossz\tMegjelenes eve\tKategoria\tEgyeb\n1\tFrozen 2\t"
+                     "103\t2019\tFamily\t6\n", ss1.str().c_str());
+        // Több találat esetén minden találatot kiír
+        coll.search("e", ss2);
+        EXPECT_STREQ("Kereses - \"e\":\nID\tCim\tHossz\tMegjelenes eve\tKategoria\tEgyeb\n1\tFrozen 2\t"
+                     "103\t2019\tFamily\t6\n2\tFree solo\t97\t2018\tDocumentary\tAlex Honnold attempts to conquer the "
+                     "first free solo climb of El Capitan's 900-metre vertical rock face.\n", ss2.str().c_str());
+    } ENDM
+
+    TEST(collection, readFile) {
+        Collection coll;
+        coll.readFile("./teszt.txt");
+        Family* familymv = (Family*) coll.getMovies()[1];
+        EXPECT_EQ(12, familymv->getAgeRating());
+        stringstream ss;
+        Documentary* docmv = (Documentary*) coll.getMovies()[2];
+        docmv->print(ss);
+        EXPECT_STREQ("2\tDokumentumfilm\t86\t2013\tDocumentary\tDokumentumfilm leirasa", ss.str().c_str());
+    } ENDM
 
     return 0;
 }
